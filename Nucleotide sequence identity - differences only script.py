4afef7fd30a205baa7f18ac966d139826661cd65
jag245 <75427784+jag245@users.noreply.github.com>
2021-01-13 13:23:07 +0000

    Add files via upload
    Files for "Machine Learning and Data Science" SWBio DTP module submission.

---
+++ Nucleotide sequence identity - differences only script.py
@@ -0,0 +1,44 @@
+#Script to evaluate differences in nucleotide identity between two sets of genomic sequence data (comparing genomic data sets from Barley yellow dwarf virus isolates.) Outputs file only contains indices for nucleotides with 0% identity and sequence gaps between genomic data sets.
+
+import pandas as pd
+
+k460_file_handle = open('Barley_Yelllow_Dwarf_Virus_Ker_III_K460_sequence.txt', 'r')
+
+k439_file_handle = open('Barley_Yellow_Dwarf_Virus_Ker_II_K439_sequence.txt', 'r')
+
+k439_sequence_string = (k439_file_handle.read())
+
+k460_sequence_string = (k460_file_handle.read())
+
+k439_file_handle.close()
+
+k460_file_handle.close()
+
+k439_sequence_list = list(k439_sequence_string)
+
+k460_sequence_list = list(k460_sequence_string)
+
+nucleotide_identity=[]
+
+nucleotide_position_index=[]
+
+while (len(k460_sequence_list))  == (len(k439_sequence_list)):
+    for index, nucleotide_k439 in enumerate(k439_sequence_list):
+        if (k439_sequence_list[index] == '-') or (k460_sequence_list[index] == '-'):
+            nucleotide_identity.append("Gap")
+            nucleotide_position_index.append(index)
+        elif k439_sequence_list[index] != k460_sequence_list[index]:
+            nucleotide_identity.append("0%, " + k439_sequence_list[index] + ":" + k460_sequence_list[index])
+            nucleotide_position_index.append(index)
+    print(nucleotide_identity)
+    print(nucleotide_position_index)
+    break   
+    
+data_dictionary = {'Index':nucleotide_position_index,'Pairwise nucleotide identity':nucleotide_identity}
+
+df = pd.DataFrame(data_dictionary)
+
+df.to_excel(excel_writer="Comparative Genomics Sequence Identity Differences.xls", sheet_name="Sequence Identity", index=False)
+
+nucleotide_identity.clear()
+nucleotide_position_index.clear()
